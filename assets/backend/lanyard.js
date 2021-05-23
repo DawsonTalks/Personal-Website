30359724aecfc1e306f9462318f482a4aa7fd49f
Jack Dawson <jackdawson2919@gmail.com>
2021-05-23 17:32:50 +0100

    Hello GitHub!

---
+++ assets/backend/lanyard.js
@@ -0,0 +1,19 @@
+async function updateLanyard() {
+    const request = await fetch('https://c.veth1.cc/at.hiven.tools:3001/status/jack');
+    const data = await request.json()
+    $('#status').text(data.status)
+    $('#name').text(data.username)
+    if (data.is_listening === false) {
+        $('.spotify').attr('style', 'display: none;')
+        $('.discord').attr('style', 'margin-top: 20px;')
+    } else if (data.is_listening === true) {
+        $('#song').text(`${data.song}`)
+        $('.spotify').attr('style', 'display: visible;')
+        $('.discord').attr('style', '')
+    }
+}
+
+updateLanyard();
+setInterval(function() {
+    updateLanyard()
+}, 5000)
